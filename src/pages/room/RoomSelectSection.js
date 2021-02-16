29a0406e2ba564a62d3f253ba14d43b3c4bf3fe8
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-02-16 13:19:00 +0530

    Fixed roomSelection postioning

--- src/pages/room/RoomSelectSection.js
+++ src/pages/room/RoomSelectSection.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Button, Flex, InputGroup, Stack } from '@chakra-ui/react';
 import { IoIosAddCircle } from 'react-icons/io';
 import RoomCard from '../../components/roomCard/RoomCard';
 
 const RoomSelectSection = () => {
   return (
-    <Flex bg='white' w='25%' h='100vh' direction='column' padding='10px'>
+    <Flex
+      as='div'
+      pos='relative'
+      bg='white'
+      w='25%'
+      h='100vh'
+      direction='column'
+      padding='10px'
+    >
       <Stack>
         <RoomCard roomName='CodeRoyale' />
         <RoomCard roomName='CodeRoyale' />
       </Stack>
-      <Button
-        rightIcon={<IoIosAddCircle color='teal' />}
-        colorScheme='teal'
-        variant='outline'
-        marginTop='10px'
-        isFullWidth={true}
+      <InputGroup
+        size='md'
+        pos='absolute'
+        bottom='0'
+        left='0'
+        padding='inherit'
       >
-        Add a Room
-      </Button>
+        <Button
+          rightIcon={<IoIosAddCircle color='teal' />}
+          colorScheme='teal'
+          variant='outline'
+          marginTop='10px'
+          isFullWidth={true}
+        >
+          Add a Room
+        </Button>
+      </InputGroup>
     </Flex>
   );
 };
