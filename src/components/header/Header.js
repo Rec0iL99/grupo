135a1012dbf0f922656d121fae6654ba296d5362
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-02-16 18:46:33 +0530

    Created header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Flex, Heading } from '@chakra-ui/react';
+
+const Header = (props) => {
+  return (
+    <Flex
+      as='nav'
+      align='center'
+      justify='space-between'
+      wrap='wrap'
+      padding='1.5rem'
+      bg='teal'
+      color='white'
+      {...props}
+    >
+      <Flex align='center' mr={5}>
+        <Heading as='h1' size='lg' letterSpacing={'-.1rem'}>
+          Grupo
+        </Heading>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
