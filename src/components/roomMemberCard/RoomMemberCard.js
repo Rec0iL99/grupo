e9743ffdd80af48ea6cd41be461eff25b9367549
Rec0iL99 <joelmathewkoshy@karunya.edu.in>
2021-03-15 00:55:30 +0530

    Added pointer for memberCard in room

--- src/components/roomMemberCard/RoomMemberCard.js
+++ src/components/roomMemberCard/RoomMemberCard.js
@@ -3,7 +3,7 @@ import { Flex, Avatar, AvatarBadge, Text } from '@chakra-ui/react';
 
 const RoomMemberCard = ({ username, profilePic, online }) => {
   return (
-    <Flex margin='5px'>
+    <Flex margin='5px' cursor='pointer'>
       <Avatar size='sm' src={profilePic}>
         <AvatarBadge boxSize='0.9em' bg={online ? 'teal' : 'gray.500'} />
       </Avatar>
